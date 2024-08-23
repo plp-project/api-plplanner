d1102d9ea73804937df49ceb03b46c435c751f2f
pedro-galdino <pedro.galdino34@gmail.com>
2024-08-22 22:58:03 -0300

    feat: done

--- src/modules/goal/goal.service.ts
+++ src/modules/goal/goal.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserService } from '../user/user.service';
 import { GoalRepository } from './infrastructure/goal.repository';
 import { CreateGoalDTO } from './interface/dto/create-goal-dto';
 import { CategoryService } from '../category/category.service';
+import { UpdateGoalDTO } from './interface/dto/update-category.dto';
 
 @Injectable()
 export class GoalService {
@@ -14,19 +15,37 @@ export class GoalService {
 
   async create(userId: number, categoryId: number, goal: CreateGoalDTO) {
     await this.userService.findOneById(userId);
-    await this.categoryService.findOneByUserId(userId, categoryId);
+    await this.categoryService.findOneByUser(userId, categoryId);
     return await this.goalRepository.create({ ...goal, userId, categoryId });
   }
 
-  async findAll(userId: number) {
+  async findAllByUser(userId: number) {
     return await this.goalRepository.find({ userId }, { category: true });
   }
 
-  async update(id: number, data: Partial<CreateGoalDTO>) {
-    return await this.goalRepository.updateById(id, data);
+  async findOneById(id: number) {
+    const goal = await this.goalRepository.findOne({ id }, { category: true });
+    if (!goal) throw new NotFoundException('Goal not found.');
+    return goal;
   }
 
-  async deleteById(id: number) {
-    return await this.goalRepository.deleteById(id);
+  async findOneByUser(userId: number, goalId: number) {
+    const goal = await this.goalRepository.findOne(
+      { id: goalId, userId },
+      { category: true }
+    );
+
+    if (!goal) throw new NotFoundException('Goal not found.');
+    return goal;
+  }
+
+  async update(userId: number, goalId: number, data: UpdateGoalDTO) {
+    await this.findOneByUser(userId, goalId);
+    return await this.goalRepository.updateById(goalId, data);
+  }
+
+  async delete(userId: number, goalId: number) {
+    await this.findOneByUser(userId, goalId);
+    return await this.goalRepository.deleteById(goalId);
   }
 }
