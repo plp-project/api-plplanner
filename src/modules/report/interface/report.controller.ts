e97466b2e69016e4012c8eb98c993600979164bf
Marcos Vinícius <mvinic2011@gmail.com>
2024-09-05 19:55:54 -0300

    feat: adding report controller

---
+++ src/modules/report/interface/report.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/modules/auth/decorators/auth.decorator';
+import { ReportService } from '../report.service';
+import { UserId } from 'src/modules/auth/decorators/user-id.decorator';
+import { CreateReportDTO } from './dto/create-report.dto';
+
+@Auth()
+@Controller('report')
+@ApiTags('Report')
+export class ReportController {
+  constructor(private readonly reportService: ReportService) {}
+
+  @Post('')
+  @ApiOperation({ summary: 'Create a report' })
+  async create(@UserId() userId: number, data: CreateReportDTO) {
+    return await this.reportService.create(userId, data.date, data.period);
+  }
+}
